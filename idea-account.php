ec4aa9e28b3a0967c7068fe7e6ea86421738692f
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-08 02:58:09 -0800

    Test

--- idea-account.php
+++ idea-account.php
@@ -10,8 +10,30 @@
 		if(mysql_num_rows($mysql_query) > 0){
 			while ($row = mysql_fetch_object($mysql_query)) {
 				$title = $row->title;	
+				$description = $row->descrip;
+				$equity = $row->equity;
+				$keywords = $row->keywords;
+				$company = $row->company;
+				$founder = $row->founder;
+				$website = $row->website;
+				$other = $row->other;
 			}	
-			echo $title . "<-";
 		}	
 	}
 ?>
+
+
+<html>
+	<div class="container">
+		<div class="jumbotron">
+			<h1><?php echo $title?></h1>
+			<p><?php echo $description?></p>
+			<p><?php echo $keywords?></p>
+			<p><?php echo $equity?></p>
+			<p><?php echo $company?></p>
+			<p><?php echo $founder?></p>
+			<p><?php echo $website?></p>
+			<p><?php echo $other?></p>
+		</div>
+	</div>
+</html>
