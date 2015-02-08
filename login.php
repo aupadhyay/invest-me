96f43580c8ff607ed229bb3bffc17fcc2f8e0329
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 16:18:45 -0800

    Update PHP

--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 <?php
 	require("../databases/config.php");
-	mysql_select_db("users");
+	mysql_select_db("invest");
 
+	$query = "SELECT `name` FROM `users` WHERE `id`='1'";
+
+	if(mysql_num_rows($query) > 0 ){
+		echo "Abhi"
+
+	}
 
 ?>
