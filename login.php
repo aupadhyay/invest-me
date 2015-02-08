3dd82d2e251d80577ed6b1129ea728809312450c
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 17:18:32 -0800

    Testing login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 		if(mysql_num_rows($query) > 0 ){
 			echo "Logged In!";
 			while($row = $query->fetch_assoc()) {
-        			echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        			echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["lname"]. "<br>";
     			}
 		}
 	}
