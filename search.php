b2e16bb7360670440baeb0db998a46dbc82c1d00
Yash-Gupta <yash0314@gmail.com>
2015-02-08 09:48:42 -0800

    Starting Over

--- search.php
+++ search.php
@@ -3,37 +3,11 @@
 	require("php/header.php");
 	require("../databases/config_users.php");
 
-	$k = $_GET['keys'];
-	$terms = explode(" ", $k);
-	$i = 0;
-	$query = "SELECT * FROM `ideas` WHERE";
-	foreach ($terms as $each){
-		$i++;
-		if($i == 1)
-			$query .= " `keywords` LIKE '%$each%' ";
-		else
-			$query .= "OR `keywords` LIKE '%$each%'";
+	$keys = $_GET['keys'];
+	$terms = explode(" ", $keys);
 
+	foreach($terms as $term){
+		echo $term;
 	}
-	echo $query;
-	$query = mysql_query($query);
-	
-	$numrows = mysql_num_rows($query);
-	
-	if($numrows > 0 ){
-		while($row = mysql_fetch_assoc($query)){
-			$title = $row['title'];
-			$desc = $row['descrip'];
-		}
-		echo $title."<br>";
-		echo $desc."<br>";
-
-
-	}else{
-		echo "No!";
-	}
-
-
-
 	
 ?>
