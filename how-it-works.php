5cacf1668d37ffe6756d7c2048cf8b10e4a79c76
Rayyaan-Mustafa <sonicshadow6@gmail.com>
2015-02-07 20:17:12 -0800

    HOw it works
    abhi told me exactly what to do but he did nothing and i did it all

---
+++ how-it-works.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	require("php/header.php");
+?>
+<html>
+	<div class="container">
+		<div class="jumbotron">
+				<h1>How It Works.</h1>
+		</div>
+	</div>
+</html>
--- logo.md
+++
@@ -1,2 +0,0 @@
-![logo27](https://cloud.githubusercontent.com/assets/9246693/6094703/1aba957e-aeeb-11e4-8a95-683b7d2a84d7.png)
-
