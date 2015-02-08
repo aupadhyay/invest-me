ec944278094f603bcf4a9eb97dfb3a9c463373d4
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 23:43:53 -0800

    Profile

--- profile.php
+++ profile.php
@@ -4,6 +4,19 @@
 		$u = $_GET['u'];
 		$sql_query = mysql_query("SELECT * FROM `users` WHERE `username`='u'");
 
+		if(mysql_num_rows($sql_query) > 0){
+			while ($row = mysql_fetch_object($sql_query)){
+				$first_name = $row->name;
+				$last_name= $row->lname;
+				$email = $row->email;
+				$location = $row->location;
+				$bio = $row->bio;
+			}
+			echo $first_name;
+		}else{
+			echo "No user with that name!";
+		}
+
 	}
 
 ?>
