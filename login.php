83bfbfe13419820713ff5439e6806c33cc512de4
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 16:22:03 -0800

    Update PHP

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 	require("../databases/config.php");
 
-	$query = "SELECT `name` FROM `users` WHERE `id`='1'";
+	$query = mysql_query("SELECT `name` FROM `users` WHERE `id`='1'");
 
 	if(mysql_num_rows($query) > 0 ){
 		echo "Abhi";
