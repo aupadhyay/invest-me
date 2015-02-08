f257e27732fcc6c2196a5924ad881a5dab98e9d7
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 22:58:01 -0800

    Tes

--- update_data.php
+++ update_data.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	require("php/header.php");
-	$id = mysql_escape_string($_SESSION["id"]);
+	$id = $_SESSION["id"];
 	$name = mysql_escape_string($_POST["first_name"]);
 	$last_name = mysql_escape_string($_POST["last_name"]);
 	$location = mysql_escape_string($_POST["location"]);
@@ -10,7 +10,7 @@
 	$sql_query = mysql_query("UPDATE INTO `users` SET `name`='$name', `lname`='$last_name', `location`='$location', `bio`='$bio' WHERE `id`='$id'");
 
 
-	
+
 
 
 ?>
