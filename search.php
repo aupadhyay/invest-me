e061dc0a49084de3707fc1045fa240992eab29b5
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-08 08:51:40 -0800

    Search I

--- search.php
+++ search.php
@@ -3,11 +3,12 @@
 	require("php/header.php");
 
 	$k = $_GET['keys'];
-
+	echo "Works"
 	$terms = explode(" ", $k);
 	$i = 0;
-	$query = "SELECT * FROM `ideas` WHERE"
+	$query = "SELECT * FROM `ideas` WHERE";
 	foreach ($terms as $each){
+		echo "yes";
 		$i++;
 		if($i == 1)
 			$query .= "`keywords` LIKE '$each' ";
