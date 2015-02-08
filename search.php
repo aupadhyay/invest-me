0f29abdf8e1a01fa0f24cf8f722e458ca8c384a6
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-08 09:05:27 -0800

    Test

--- search.php
+++ search.php
@@ -17,16 +17,15 @@
 	}
 
 	$query = mysql_query($query);
-	echo $query;
 	$numrows = mysql_num_rows($query);
 	
 	if($numrows > 0 ){
-		echo "testing";
 		while($row = mysql_fetch_assoc($query)){
 			$title = $row['title'];
 			$desc = $row['descrip'];
 		}
 		echo $title."<br>";
+		echo $desc."<br>";
 
 
 	}else{
