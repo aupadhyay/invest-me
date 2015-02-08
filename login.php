672087cbc58db4a801e7ad234849caf1b3815e2b
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 16:43:35 -0800

    Bug fix login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 	$_POST['email'] = "abhi.upadhyay01@gmail.com";
 	$_POST['pass'] = "janoneseven2";
 	if(isset($_POST['email'])){
-		w30 $username = mysql_escape_string($_POST['email']);
+		$username = mysql_escape_string($_POST['email']);
 		$pass = mysql_escape_string($_POST['pass']);
 		$query = mysql_query("SELECT * FROM `users` WHERE `email`='$username' AND `pass`='$pass'");
 
