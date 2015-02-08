33fb65c60b020879625978b9d2fb2eab9a2b2af9
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 20:42:18 -0800

    Bug fix image

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
           <div class="col-sm-6">
             <h1>Investors</h1>
             <p>Come look at cool products and invest in them! Help businesses grow with your business expertise!</p>
-            <img src="img/money.png" alt="Money">
+            <img src="img/money.png" alt="Money" width="100" height="100">
           </div>
           
           <div class="col-sm-6">
