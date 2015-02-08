4570dcecd289f327b2b8c97b33c444f9350bb895
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 17:57:33 -0800

    Test

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
 	if(isset($_POST['email'])){
 		$username = mysql_escape_string($_POST['email']);
 		$pass = mysql_escape_string($_POST['pass']);
+		echo $pass;
 		$query = mysql_query("SELECT * FROM `users` WHERE `email`='$username' AND `pass`='$pass'");
 
 		if(mysql_num_rows($query) > 0 ){
