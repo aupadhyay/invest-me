1d33e5ae09130dfdb646f6abad5aaefc01a6be6e
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 16:42:54 -0800

    Updates

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <?php
 	require("../databases/config_users.php");
+	$_POST['email'] = "abhi.upadhyay01@gmail.com";
+	$_POST['pass'] = "janoneseven2";
 	if(isset($_POST['email'])){
-		$username = mysql_escape_string($_POST['email']);
+		w30 $username = mysql_escape_string($_POST['email']);
 		$pass = mysql_escape_string($_POST['pass']);
 		$query = mysql_query("SELECT * FROM `users` WHERE `email`='$username' AND `pass`='$pass'");
 
