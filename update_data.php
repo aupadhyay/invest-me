a742fd226e60fc5457319863f3519115d05d2fe2
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-08 11:31:22 -0800

    Test

--- update_data.php
+++ update_data.php
@@ -1,11 +1,6 @@
 <?php
 	session_start();
 	require("../databases/config_users.php");
-	function change_profile_image($user_id, $file_temp){
-		echo "Abhi!";
-		$file_path = 'img/profile/'. substr(md5(time()), 0,10). '.png';
-		move_uploaded_file($file_temp, $file_path);
-	}
 
 	$id = $_SESSION["id"];
 	$name = mysql_escape_string($_POST["fname"]);
@@ -14,9 +9,6 @@
 	$bio =mysql_escape_string($_POST["bio"]);
 
 	$sql_query = mysql_query("UPDATE `users` SET `name`='$name', `lname`='$last_name', `location`='$location', `bio`='$bio' WHERE `ID`='$id'");
-	$file_name = $_FILES['profile']['name'];
-	$file_temp = $_FILES['profile']['tmp_name'];
-	change_profile_image($_SESSION['id'], $file_temp);
 	$_SESSION["first_name"] = $name;
 	$_SESSION["last_name"] = $last_name;
 	$_SESSION["location"] = $location;
