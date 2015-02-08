5455ede3e65f0d81b6d9d8260a9b55bf0200234e
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-08 03:46:19 -0800

    Added trash

--- profile.php
+++ profile.php
@@ -63,7 +63,7 @@
 				<img src="http://placehold.it/30x30" alt="A">
 			</div>
 			<div class="col-sm-3">
-				<img src="http://placehold.it/30x30" alt="A">
+				<img src="img/trash.jpg" alt="A">
 			</div>
 			<div class="col-sm-12">
 				<hr>
