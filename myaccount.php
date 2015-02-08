f083a2bd3c932e3f739636939aa070d27c41919d
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-02-07 22:16:49 -0800

    Test

--- myaccount.php
+++ myaccount.php
@@ -13,17 +13,17 @@
 			<form action="update_data.php" method="POST">
 				<div class="form-control">
 					<label for="name">First Name</label>
-					<input type="text" id="first_name" name= "first_name"placeholder=<?php echo $_SESSION["first_name"]; ?>>
+					<input type="text" id="first_name" name= "first_name"value=<?php echo $_SESSION["first_name"]; ?>>
 					<label for="last_name">Last Name</label>
-					<input type="text" id="last_name" name= "last_name"placeholder=<?php echo $_SESSION["last_name"]; ?>>
+					<input type="text" id="last_name" name= "last_name"value=<?php echo $_SESSION["last_name"]; ?>>
 					<label for="location">Location</label>
-					<input type="text" id="location" name= "location"placeholder=<?php echo $location; ?>>
+					<input type="text" id="location" name= "location"value=<?php echo $location; ?>>
 					<label for="phone">Phone</label>
-					<input type="text" id="phone" name= "phone"placeholder=<?php echo $_SESSION["phone"]; ?>>
+					<input type="text" id="phone" name= "phone"value=<?php echo $_SESSION["phone"]; ?>>
 					<label for="hours">Hours</label>
-					<input type="text" id="hours" name= "hours"placeholder=<?php echo $_SESSION["hours"]; ?>>
+					<input type="text" id="hours" name= "hours"value=<?php echo $_SESSION["hours"]; ?>>
 					<label for="bio">Bio</label>
-					<input type="textarea" id="bio" name= "bio"placeholder=<?php echo $_SESSION["bio"]; ?>>
+					<input type="textarea" id="bio" name= "bio"value=<?php echo $_SESSION["bio"]; ?>>
 				</div>
 			</form>
 
