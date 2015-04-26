2bdc94bb9cbb46136b5de53ee2dc8295132de37b
Yash-Gupta <yash0314@gmail.com>
2015-04-26 11:36:16 -0700

    changed to invest.me

--- how-it-works.php
+++ how-it-works.php
@@ -8,7 +8,7 @@
 				<h1>How It Works.</h1>
 				<hr>
 				<p style="font-size:30px">Are you a venture capitalist? OR do you need captial to really <em>ignite</em> your business? <br>
-					E-Vest is the place for you!</p>
+					Invest.me is the place for you!</p>
 				<ul>
 					<li><STRONG>Venture Capitalists</STRONG> </li>
 						<ul>
